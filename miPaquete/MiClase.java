3b6f541e6e07f8fae6392b0604a8636542fb9d5c
JonathanDiz <jdiazolivares007@gmail.com>
2023-04-02 12:36:45 -0400

    first commit

---
+++ miPaquete/MiClase.java
@@ -0,0 +1,20 @@
+package miPaquete;
+
+public class MiClase {
+
+    public static void main(String[] args) {
+        int entero = 10;
+        long largo = 123456789L;
+        double doble = 3.141592653589793d;
+        float flotante = 1.5f;
+        boolean booleano = true;
+        String cadena = "Hola Mundo!";
+
+        System.out.println("Entero: " + entero);
+        System.out.println("Largo: " + largo);
+        System.out.println("Doble: " + doble);
+        System.out.println("Flotante: " + flotante);
+        System.out.println("Booleano: " + booleano);
+        System.out.println("Cadena:" + cadena);
+    }
+}
